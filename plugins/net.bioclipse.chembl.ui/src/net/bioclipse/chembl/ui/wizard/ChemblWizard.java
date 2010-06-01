b29733b237783fb5d66109727705a6cb5432448d
annzi <annzi.andersson@gmail.com>
2010-06-01 14:15:50 +0200

    Implements INewWizard

--- plugins/net.bioclipse.chembl.ui/src/net/bioclipse/chembl/ui/wizard/ChemblWizard.java
+++ plugins/net.bioclipse.chembl.ui/src/net/bioclipse/chembl/ui/wizard/ChemblWizard.java
@@ -5,31 +5,44 @@ import net.bioclipse.chembl.Activator;
 import net.bioclipse.chembl.business.IChEMBLManager;
 import org.apache.log4j.Logger;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
 
-public class ChemblWizard extends Wizard {
+public class ChemblWizard extends Wizard implements INewWizard {
 
-	private static final Logger logger = Logger.getLogger(ChemblWizard.class);
-	private ChemblWizardPage firstpage;
-	private IChEMBLManager chembl;
+    private static final Logger logger = Logger.getLogger(ChemblWizard.class);
+    private ChemblWizardPage firstpage;
+    private IChEMBLManager chembl;
     //ChemblData data = new ChemblData();
-  
-	public ChemblWizard() {
-		super();
-		setWindowTitle("ChEMBL wizard");
-		chembl = Activator.getDefault().getJavaChEMBLManager();
-	}
+ 
+    public ChemblWizard() {
+        super();
+        setWindowTitle("ChEMBL wizard");
+        chembl = Activator.getDefault().getJavaChEMBLManager();
+    }
 
-	public void addPages(){
-		firstpage = new ChemblWizardPage("ChEMBL page");
-		addPage(firstpage);
-	}	
+    public void addPages(){
+        firstpage = new ChemblWizardPage("ChEMBL page");
+        addPage(firstpage);
+    }    
 
-	//Writes compounds to a MoSS supported file
+    //Writes compounds to a MoSS supported file
+    @Override
+    public boolean performFinish() {
+    
+        return false;
+    }
+    @ Override
+    public boolean isHelpAvailable() {
+        return true;
+    }
+
 	@Override
-	public boolean performFinish() {
-	
-		return false;
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+		
 	}
-	
 }
+
